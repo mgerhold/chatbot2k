32bc61dbfbe0d775498b9b257002934ccb5cea4d
Michael Gerhold <michael.gerhold@web.de>
2026-02-14 13:48:39 +0100

    feat: do not trigger soundboard and entrance sounds for the same message

--- src/chatbot2k/entrance_sounds.py
+++ src/chatbot2k/entrance_sounds.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import logging
 from typing import Final
+from typing import Optional
 from typing import final
 
 from chatbot2k.app_state import AppState
@@ -12,31 +15,60 @@ logger: Final = logging.getLogger(__name__)
 
 @final
 class EntranceSoundHandler:
+    @final
+    class _Passkey: ...
+
+    @final
+    class EntranceSoundCommand:
+        def __init__(
+            self,
+            handler: EntranceSoundHandler,
+            *,
+            sender_twitch_user_id: str,
+            clip_url: str,
+            _passkey: EntranceSoundHandler._Passkey,
+        ) -> None:
+            self._handler: Final = handler
+            self._sender_twitch_user_id: Final = sender_twitch_user_id
+            self._clip_url: Final = clip_url
+
+        async def trigger(self) -> None:
+            self._handler._entrance_sounds_already_played_for.add(self._sender_twitch_user_id)
+            await self._handler._app_state.enqueue_soundboard_clip_url(self._clip_url)
+
     def __init__(self, app_state: AppState) -> None:
         self._app_state: Final = app_state
         self._entrance_sounds_already_played_for: Final[set[str]] = set()
 
-    async def trigger_entrance_sounds(
+    async def get_entrance_sound(
         self,
         chat_message: ChatMessage,
-    ) -> None:
+    ) -> Optional[EntranceSoundCommand]:
+        """
+        Triggers entrance sounds for a chat message if applicable. Returns `True`
+        if an entrance sound was triggered, otherwise `False`.
+        """
         metadata: Final = chat_message.meta_data
         if not isinstance(metadata, TwitchChatMessageMetadata):
             logger.error("Entrance sounds are only supported for Twitch chat messages.")
-            return
+            return None
         sender_twitch_user_id: Final = metadata.message.user.id
         if sender_twitch_user_id in self._entrance_sounds_already_played_for:
             # This user has already had their entrance sound played during this session.
-            return
+            return None
         entrance_sound: Final = self._app_state.database.get_entrance_sound_by_twitch_user_id(
             twitch_user_id=sender_twitch_user_id
         )
         if entrance_sound is None:
-            return
+            return None
 
-        self._entrance_sounds_already_played_for.add(sender_twitch_user_id)
         clip_url: Final = f"/{RELATIVE_SOUNDBOARD_FILES_DIRECTORY.as_posix()}/{entrance_sound.filename}"
-        await self._app_state.enqueue_soundboard_clip_url(clip_url)
+        return EntranceSoundHandler.EntranceSoundCommand(
+            handler=self,
+            sender_twitch_user_id=sender_twitch_user_id,
+            clip_url=clip_url,
+            _passkey=EntranceSoundHandler._Passkey(),
+        )
 
     def reset_entrance_sounds_session(self) -> None:
         """Resets the entrance sounds session, allowing entrance sounds to be played again for all users."""
